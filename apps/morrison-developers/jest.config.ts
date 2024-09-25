68d4a48e92009ad10e7a5af5f1043870fd36e1bc
Andrew Morrison <morrison.andrew422@gmail.com>
2024-09-25 10:10:20 -0400

    fix linting errors

--- apps/morrison-developers/jest.config.ts
+++ apps/morrison-developers/jest.config.ts
@@ -1,4 +1,4 @@
-export default {
+const config = {
   displayName: 'morrison-developers',
   preset: '../../jest.preset.js',
   transform: {
@@ -8,3 +8,5 @@ export default {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/morrison-developers',
 };
+
+export default config;
--- apps/morrison-developers/src/app/registry.tsx
+++ apps/morrison-developers/src/app/registry.tsx
@@ -9,17 +9,10 @@ export function StyledComponentsRegistry({
 }: {
   children: React.ReactNode;
 }) {
-  // Only create stylesheet once with lazy initial state
-  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
 
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
-
-    // Types are out of date, clearTag is not defined.
-    // See: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/65021
-    (styledComponentsStyleSheet.instance as any).clearTag();
-
     return <>{styles}</>;
   });
 
