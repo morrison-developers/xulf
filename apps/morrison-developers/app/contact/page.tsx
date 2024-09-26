90557251d35229e9cffd96e1031b165e0d2e4005
Andrew Morrison <morrison.andrew422@gmail.com>
2024-09-25 20:04:37 -0400

    add contact page

---
+++ apps/morrison-developers/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import { metadata as contactMetadata } from './metadata';
+
+export const metadata = contactMetadata;
+
+export default function ContactPage() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+}
