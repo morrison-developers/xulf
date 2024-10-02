9a88362ebc60c131fee3faf1b463987c29a18f42
Andrew Morrison <morrison.andrew422@gmail.com>
2024-10-02 13:28:16 -0400

    update absolutley positioned container to accept svgs

--- libs/layouts/src/absolutley-positioned-container/absolutley-positioned-container.tsx
+++ libs/layouts/src/absolutley-positioned-container/absolutley-positioned-container.tsx
@@ -7,17 +7,34 @@ interface ContainerProps {
 
 const StyledAbsolutleyPositionedContainer = styled.div<ContainerProps>`
   position: absolute;
-  top: ${props => props.top || '0'};
-  left: ${props => props.left || '0'};
+  top: ${props => props.top || '50%'};
+  left: ${props => props.left || '50%'};
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: visible;
+`;
+
+const SVGWrapper = styled.div`
+  position: absolute;
+  z-index: -1;
 `;
 
 export interface AbsolutleyPositionedContainerProps extends ContainerProps {
   children?: React.ReactNode;
+  svg?: React.ReactNode;
 }
 
-export function AbsolutleyPositionedContainer({ top, left, children }: AbsolutleyPositionedContainerProps) {
+export function AbsolutleyPositionedContainer({ 
+  top, 
+  left, 
+  children, 
+  svg 
+}: AbsolutleyPositionedContainerProps) {
   return (
     <StyledAbsolutleyPositionedContainer top={top} left={left}>
+      {svg && <SVGWrapper>{svg}</SVGWrapper>}
       {children}
     </StyledAbsolutleyPositionedContainer>
   );
