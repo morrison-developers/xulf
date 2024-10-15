b7eb1fc760bfe95f59c54f7016598ef9aa1ec400
Andrew Morrison <morrison.andrew422@gmail.com>
2024-10-15 10:10:07 -0400

    MOR-43

--- apps/morrison-developers/app/page.tsx
+++ apps/morrison-developers/app/page.tsx
@@ -2,6 +2,7 @@
 
 import styled from 'styled-components';
 import { FloatingNavBar, ThemeContextProvider } from '@xulf/ui';
+import { AbsolutleyPositionedContainer } from '@xulf/layouts';
 
 const StyledPage = styled.div`
   .page {
@@ -9,15 +10,21 @@ const StyledPage = styled.div`
   }
 `;
 
+const StyledBackground1a = styled.div`
+  position: absolute;
+  z-index: 2;
+`;
+
+const StyledBackground1b = styled.div`
+  position: absolute;
+  z-index: 1;
+`;
+
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.styled-components file.
-   */
   return (
     <ThemeContextProvider>
       <StyledPage>
+        {/* NAV BAR */}
         <FloatingNavBar>
           <h1>Home</h1>
           <h1>About</h1>
@@ -26,6 +33,23 @@ export default function Index() {
           <h1>Services</h1>
           <h1>Projects</h1>
         </FloatingNavBar>
+        {/* NAV BAR */}
+
+        {/* BACKGROUND */}
+        <StyledBackground1a>
+          <AbsolutleyPositionedContainer
+            svg={'/bg-svgs/geometric-1a.svg'}
+          />
+        </StyledBackground1a>
+        <StyledBackground1b>
+          <AbsolutleyPositionedContainer 
+            svg={'/bg-svgs/geometric-1b.svg'}
+          />
+        </StyledBackground1b>
+        {/* BACKGROUND */}
+
+        {/* FOREGROUND */}
+        {/* FOREGROUND */}
       </StyledPage>
     </ThemeContextProvider>
   );
