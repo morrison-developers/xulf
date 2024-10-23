a22ad652c76b7cca6b4fde3206af54d57e3d8d56
Andrew Morrison <morrison.andrew422@gmail.com>
2024-10-23 16:30:24 -0400

    add favicon

--- apps/nicoletta-portfolio/src/app/layout.tsx
+++ apps/nicoletta-portfolio/src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon@2x.png" />
+      </head>
       <body>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
