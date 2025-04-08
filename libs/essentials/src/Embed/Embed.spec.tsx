9fa8571b6ea3edb6e6873831ebe0368de3d91472
Andrew Morrison <morrison.andrew422@gmail.com>
2025-04-08 17:26:58 -0400

    fix failing tests

--- libs/essentials/src/Embed/Embed.spec.tsx
+++ libs/essentials/src/Embed/Embed.spec.tsx
@@ -1,10 +1,9 @@
 import { render } from '@testing-library/react';
-
 import Embed from './Embed';
 
 describe('Embed', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Embed />);
+    const { baseElement } = render(<Embed embedContent="<div>Yo</div>" />);
     expect(baseElement).toBeTruthy();
   });
 });
--- libs/essentials/src/Modal/Modal.spec.tsx
+++ libs/essentials/src/Modal/Modal.spec.tsx
@@ -1,10 +1,20 @@
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
-import Modal from './Modal';
+import { Modal } from './Modal';
+import { ButtonOverlay } from '../ButtonOverlay/ButtonOverlay'
 
 describe('Modal', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Modal />);
-    expect(baseElement).toBeTruthy();
+  it('should render modal after button click', () => {
+    render(
+      <>
+        <Modal id="test">Test Modal Content</Modal>
+        <ButtonOverlay modalTargetId="test">Open Modal</ButtonOverlay>
+      </>
+    );
+
+    fireEvent.click(screen.getByText('Open Modal'));
+
+    const modalContent = screen.queryByText('Test Modal Content');
+    expect(modalContent).not.toBeNull();
   });
 });
