b91fe6905621e9a0430091a78a3d2f8cb366751f
Andrew Morrison <morrison.andrew422@gmail.com>
2025-04-10 17:00:12 -0400

    MOR-202 add POST /api/orgs endpoint

--- apps/xulf-ui/xulf-ui/src/app/api/auth/[...nextauth]/route.ts
+++ apps/xulf-ui/xulf-ui/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth";
-import { authConfig } from "../config";
+import NextAuth from 'next-auth';
+import { authConfig } from '../config';
 
 const handler = NextAuth(authConfig);
 
