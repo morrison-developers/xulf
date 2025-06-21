132bbc9ac66e65eab489171f016da8b053311b0e
Andrew Morrison <morrison.andrew422@gmail.com>
2025-06-21 14:50:02 -0400

    fix

--- apps/morrison-developers/morrison-developers-app/next.config.js
+++ apps/morrison-developers/morrison-developers-app/next.config.js
@@ -1,37 +1,35 @@
 //@ts-check
 
 const { composePlugins, withNx } = require('@nx/next');
+const path = require('path');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
     svgr: false,
   },
 
   compiler: {
-    // For other options, see https://styled-components.com/docs/tooling#babel-plugin
     styledComponents: true,
   },
+
   experimental: {
     serverActions: {},
-    outputFileTracingIncludes: {
-      '/app/api/': [
-        'node_modules/@prisma/client/**/*',
-        'node_modules/.prisma/client/**/*',
-        'libs/mor-dev-db/src/lib/generated/morrison-prisma-client/**/*',
-      ],
-    },
-    outputFileTracingRoot: require('path').join(__dirname, '../../../'),
   },
+
+  outputFileTracingIncludes: {
+    'apps/morrison-developers/morrison-developers-app': [
+      'libs/mor-dev-db/src/lib/generated/morrison-prisma-client',
+      'node_modules/@prisma/client',
+      'node_modules/.prisma/client'
+    ]
+  },
+
+  outputFileTracingRoot: path.join(__dirname, '../../../'),
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
+const plugins = [withNx];
 
 module.exports = composePlugins(...plugins)(nextConfig);
