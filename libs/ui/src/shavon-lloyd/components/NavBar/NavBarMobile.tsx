d3c0ecaad8d98fad4360e5415558c6432fb32553
Andrew Morrison <morrison.andrew422@gmail.com>
2025-09-27 18:55:38 -0400

    fix svg attr names

--- libs/ui/src/shavon-lloyd/components/NavBar/NavBarMobile.tsx
+++ libs/ui/src/shavon-lloyd/components/NavBar/NavBarMobile.tsx
@@ -78,11 +78,11 @@ export default function NavBarMobile() {
         >
           {!open ?
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-              <path d="M3 12H17M3 6H21M3 18H21" stroke="#D7E2EB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M3 12H17M3 6H21M3 18H21" stroke="#D7E2EB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
           :
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-              <path d="M18 6L6 18M6 6L18 18" stroke="#D7E2EB" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              <path d="M18 6L6 18M6 6L18 18" stroke="#D7E2EB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
           }
         </div>
